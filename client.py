efb67f4446fda28527a53e7f739669161806f23c
Sorin <szele@tudelft.nl>
2026-04-25 20:17:10 +0200

    Typo

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ from ipv8_service import IPv8
 
 EMAIL = "szele@student.tudelft.nl"
 GITHUB_URL = "https://github.com/ZeleSorin/BEA1#"
-NONCE = 211495735  # TODO: fill in your mined nonce here
+NONCE = 211495735  
 
 SERVER_KEY_HEX = "4c69624e61434c504b3a86b23934a28d669c390e2d1fc0b0870706c4591cc0cb178bc5a811da6d87d27ef319b2638ef60cc8d119724f4c53a1ebfad919c3ac4136c501ce5c09364e0ebb"
 COMMUNITY_ID = "2c1cc6e35ff484f99ebdfb6108477783c0102881"
